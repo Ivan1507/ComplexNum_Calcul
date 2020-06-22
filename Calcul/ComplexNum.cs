bdf29861106352721792fcda50fd3fc7055da905
Ivan <iancurtis1507@mail.ru>
2020-06-22 17:10:05 +0300

    done

---
+++ Calcul/ComplexNum.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcul
+{
+    public class ComplexNum
+    {
+        private double real;
+        private double image;
+
+        public ComplexNum()
+        {
+            real = 1;
+            image = 1;
+        }
+        public ComplexNum(double re)
+        {
+            real = re;
+            image = 0;
+        }
+        public ComplexNum(double re, double im)
+        {
+            real = re;
+            image = im;
+        }
+
+        public void setReal(double re)
+        {
+            real = re;
+        }
+        public void setImage(double im)
+        {
+            image = im;
+        }
+        public double getReal()
+        {
+            return real;
+        }
+        public double getImage()
+        {
+            return image;
+        }
+
+        static public ComplexNum operator +(ComplexNum left, ComplexNum right)
+        {
+            ComplexNum complexNum = new ComplexNum();
+            complexNum.setReal(left.getReal() + right.getReal());
+            complexNum.setImage(left.getImage() + right.getImage());
+            return complexNum;
+        }
+        static public ComplexNum operator -(ComplexNum left, ComplexNum right)
+        {
+            ComplexNum complexNum = new ComplexNum();
+            complexNum.setReal(left.getReal() - right.getReal());
+            complexNum.setImage(left.getImage() - right.getImage());
+            return complexNum;
+        }
+        static public ComplexNum operator *(ComplexNum left, ComplexNum right)
+        {
+            ComplexNum complexNum = new ComplexNum();
+            complexNum.setReal(left.getReal() * right.getReal() - left.getImage() * right.getImage());
+            complexNum.setImage(left.getReal() * right.getImage() + left.getImage() * right.getReal());
+            return complexNum;
+        }
+        static public ComplexNum operator *(ComplexNum left, double right)
+        {
+            ComplexNum complexNum = new ComplexNum();
+            complexNum.setReal(left.getReal() * right);
+            complexNum.setImage(left.getImage() * right);
+            return complexNum;
+        }
+        static public ComplexNum operator *(double left, ComplexNum right)
+        {
+            ComplexNum complexNum = new ComplexNum();
+            complexNum.setReal(left * right.getReal());
+            complexNum.setImage(left * right.getImage());
+            return complexNum;
+        }
+        static public ComplexNum operator /(ComplexNum left, ComplexNum right)
+        {
+            ComplexNum complexNum = new ComplexNum();
+            double r1 = left.getReal();
+            double r2 = right.getReal();
+            double im1 = left.getImage();
+            double im2 = right.getImage();
+            double r = (r1 * r2 + im1 * im2) / (r2 * r2 + im2 * im2);
+            double im = (r2 * im1 - r1 * im2) / (r2 * r2 + im2 * im2);
+            complexNum.setReal(r);
+            complexNum.setImage(im);
+            return complexNum;
+        }
+        static public ComplexNum operator /(ComplexNum left, double right)
+        {
+            ComplexNum complexNum = new ComplexNum();
+            complexNum.setReal(left.getReal() / right);
+            complexNum.setImage(left.getImage() / right);
+            return complexNum;
+        }
+        public void Convert(String str)
+        {
+            String buf1 = "", buf2 = "";
+            int i = 0;
+
+            for (; i < str.Length; i++)
+            {
+                if (str[i] == '+' || str[i] == '-') break;
+                else buf1 += str[i];
+            }
+            if(str[i]=='+') i += 1;
+
+            for (; i < str.Length; i++)
+            {
+                if (str[i] == 'i') break;
+                else buf2 += str[i];
+            }
+
+            this.setReal(Double.Parse(buf1));
+            this.setImage(Double.Parse(buf2));
+        }
+       public String BackConvert()
+        {
+            if (this.getImage() >= 0)
+            {
+                return this.getReal().ToString() + '+' + this.getImage().ToString() + 'i';
+            }
+            else
+            {
+                return this.getReal().ToString() + '-' + (this.getImage()).ToString() + 'i';
+            }
+        }
+    }
+}
---
+++ Calcul/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ComplexNum c1 = new ComplexNum(0, 0);
+        int i = 0;
+        bool f = false;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+            //textBox1.Text += "/";
+            i = 4;
+          
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            
+            if(f) textBox2.Text += "i";
+            else textBox1.Text += "i";
+            f = true;
+
+        }
+     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (f) textBox2.Text += "1";
+            else textBox1.Text += "1";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (f) textBox2.Text += "2";
+            else textBox1.Text += "2";
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            comboBox1.Text = "";
+            f = false;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if(f) textBox2.Text += "3";
+            else textBox1.Text += "3";
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            if (f) textBox2.Text += "4";
+            else textBox1.Text += "4";
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            if (f) textBox2.Text += "5";
+            else textBox1.Text += "5";
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            if (f) textBox2.Text += "6";
+            else textBox1.Text += "6";
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            if (f) textBox2.Text += "7";
+            else textBox1.Text += "7";
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            if (f) textBox2.Text += "8";
+            else textBox1.Text += "8";
+
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            if (f) textBox2.Text += "9";
+            else textBox1.Text += "9";
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            if (f) textBox2.Text += "0";
+            else textBox1.Text += "0";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (f) textBox2.Text += "+";
+            else textBox1.Text += "+";
+            i = 1;
+      
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (f) textBox2.Text += "-";
+            else textBox1.Text += "-";
+            i = 2;
+           
+           
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "*";
+            i = 3;
+           
+        }
+
+       private void button11_Click(object sender, EventArgs e)
+        {
+           
+            var cmp1 = new ComplexNum();
+            cmp1.Convert(textBox1.Text);
+            var cmp2 = new ComplexNum();
+            cmp2.Convert(textBox2.Text);
+           
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "+": cmp1 += cmp2;
+                    break;
+                case "-":cmp1-=cmp2;
+                    break;
+                case "*":cmp1 *=cmp2;
+                    break;
+                case "/":cmp1/=cmp2;
+                    break;
+                default:
+                    MessageBox.Show("Вы не выбрали операцию!");
+                    break;
+            }
+            textBox3.Text = cmp1.BackConvert();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
